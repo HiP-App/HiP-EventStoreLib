57d5ce126c377f9b42d402887db2c784f207c85c
Sven Erik Vinkemeier <svenvinkemeier@live.de>
2017-09-06 16:20:42 +0200

    Added basic MongoDB constructs

---
+++ HiP-EventStoreLib/Mongo/DocRefBase.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace PaderbornUniversity.SILab.Hip.EventSourcing.Mongo
+{
+    /// <summary>
+    /// Base class for <see cref="DocRef{T}"/> and <see cref="DocRefList{T}"/>.
+    /// </summary>
+    public abstract class DocRefBase
+    {
+        /// <summary>
+        /// The name of the collection where the referenced document is. This property is optional:
+        /// If not set, the referenced document is assumed to be in the same collection as the referencing document.
+        /// </summary>
+        [BsonElement]
+        public string Collection { get; private set; }
+
+        /// <summary>
+        /// The name of the database where the referenced document is. This property is optional:
+        /// If not set, the referenced document is assumed to be in the same database as the referencing document.
+        /// </summary>
+        [BsonElement]
+        public string Database { get; private set; }
+
+        protected DocRefBase()
+        {
+        }
+
+        protected DocRefBase(string collection, string database)
+        {
+            Collection = collection;
+            Database = database;
+        }
+    }
+}
---
+++ HiP-EventStoreLib/Mongo/DocRefList.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaderbornUniversity.SILab.Hip.EventSourcing.Mongo
+{
+    /// <summary>
+    /// A strongly-typed reference to multiple other documents in a Mongo database.
+    /// </summary>
+    /// <remarks>
+    /// Unfortunately this type cannot implement <see cref="IEnumerable{BsonValue}"/> or any derived type
+    /// because the BSON serializer then serializes objects of this type as array, forgetting about the fields
+    /// 'Collection' and 'Database'.
+    /// </remarks>
+    public class DocRefList<T> : DocRefBase//, ICollection<BsonValue>
+    {
+        [BsonElement]
+        public OrderedSet<BsonValue> Ids { get; private set; } = new OrderedSet<BsonValue>();
+
+        public int Count => Ids.Count;
+
+        public DocRefList()
+        {
+        }
+
+        public DocRefList(string collection = null, string database = null) : base(collection, database)
+        {
+        }
+
+        public DocRefList(IEnumerable<BsonValue> ids, string collection = null, string database = null) : base(collection, database)
+        {
+            if (ids == null)
+                throw new ArgumentNullException(nameof(ids));
+
+            Ids = new OrderedSet<BsonValue>(ids);
+        }
+
+        public bool Add(BsonValue id) => Ids.Add(id);
+
+        public void Add(IEnumerable<BsonValue> ids)
+        {
+            foreach (var id in ids ?? Enumerable.Empty<BsonValue>())
+                Ids.Add(id);
+        }
+
+        public bool Remove(BsonValue id) => Ids.Remove(id);
+
+        public void Clear() => Ids.Clear();
+
+        public bool Contains(BsonValue id) => Ids.Contains(id);
+
+        public IEnumerator<BsonValue> GetEnumerator() => Ids.GetEnumerator();
+    }
+}
