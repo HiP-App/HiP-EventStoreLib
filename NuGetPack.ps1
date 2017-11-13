a1acb1150192041c9820a8d21c9cb3cdfc104536
Sven Vinkemeier <svenvinkemeier@live.de>
2017-11-13 14:28:31 +0100

    Added script for packaging & publishing

---
+++ NuGetPack.ps1
@@ -0,0 +1,8 @@
+Switch ("$env:Build_SourceBranchName")
+{
+	"master" { dotnet pack "HiP-EventStoreLib\HiP-EventStoreLib.csproj" -o . }
+	"develop" { dotnet pack "HiP-EventStoreLib\HiP-EventStoreLib.csproj" -o . --version-suffix "develop" }
+}
+
+$nupkg = (ls HiP-EventStoreLib\*.nupkg).FullName
+dotnet nuget push "$nupkg" -k "$MyGetKey" -s "$MyGetFeed"
