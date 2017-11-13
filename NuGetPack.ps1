8a46d5e414517b584d7bacf7a9757b6977bdf503
Sven Vinkemeier <svenvinkemeier@live.de>
2017-11-13 15:08:56 +0100

    Added default case

--- NuGetPack.ps1
+++ NuGetPack.ps1
@@ -2,6 +2,7 @@ Switch ("$env:Build_SourceBranchName")
 {
 	"master" { dotnet pack "HiP-EventStoreLib\HiP-EventStoreLib.csproj" -o . }
 	"develop" { dotnet pack "HiP-EventStoreLib\HiP-EventStoreLib.csproj" -o . --version-suffix "develop" }
+	default { exit }
 }
 
 $nupkg = (ls HiP-EventStoreLib\*.nupkg).FullName
