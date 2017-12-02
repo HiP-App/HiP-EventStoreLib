fdd22c2cb95d6f309338049ea53f2fbee4c98049
Sven Vinkemeier <svenvinkemeier@live.de>
2017-12-02 15:38:48 +0100

    Updated NuGetPack-script

--- NuGetPack.ps1
+++ NuGetPack.ps1
@@ -1,9 +1,11 @@
+$csproj = (ls HiP-EventStoreLib\*.csproj).FullName
 Switch ("$env:Build_SourceBranchName")
 {
-	"master" { dotnet pack "HiP-EventStoreLib\HiP-EventStoreLib.csproj" -o . }
-	"develop" { dotnet pack "HiP-EventStoreLib\HiP-EventStoreLib.csproj" -o . --version-suffix "develop" }
-	default { exit }
+	"master" { dotnet pack "$csproj" -o . }
+    "develop" { dotnet pack "$csproj" -o . --version-suffix "develop" }
+    "nuget" { dotnet pack "$csproj" -o . --version-suffix "develop" }
+    default { exit }
 }
-
 $nupkg = (ls HiP-EventStoreLib\*.nupkg).FullName
-dotnet nuget push "$nupkg" -k "$env:MyGetKey" -s "$env:NuGetFeed"
+dotnet --% nuget push "$nupkg" -k %MyGetKey% -s %NuGetFeed%
+$LASTEXITCODE = 0
