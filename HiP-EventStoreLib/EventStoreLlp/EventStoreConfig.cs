aebb008404d7a8de49ea7ca99bfc050c11ea6378
Sven Vinkemeier <svenvinkemeier@live.de>
2017-12-12 11:58:26 +0100

    EventStoreConfig properties are now virtual so that default values can be overridden, v1.1.1

--- HiP-EventStoreLib/EventStoreLlp/EventStoreConfig.cs
+++ HiP-EventStoreLib/EventStoreLlp/EventStoreConfig.cs
@@ -7,6 +7,7 @@
     {
         /// <summary>
         /// Endpoint of the Event Store.
+        /// Default value: "tcp://localhost:1113"
         /// 
         /// Examples:
         /// "tcp://localhost:1113",
@@ -15,12 +16,12 @@
         /// 
         /// See also: http://docs.geteventstore.com/dotnet-api/4.0.0/connecting-to-a-server/
         /// </summary>
-        public string Host { get; set; }
+        public virtual string Host { get; set; } = "tcp://localhost:1113";
 
         /// <summary>
         /// Name of the event stream to read from and write to.
         /// For example, you can use different streams for develop and production environments.
         /// </summary>
-        public string Stream { get; set; }
+        public virtual string Stream { get; set; }
     }
 }
