c8784bce37f0a28511026d002ad735c3346157cc
Sven Erik Vinkemeier <sven@vinkemeier.de>
2017-12-12 15:20:03 +0100

    Default value for EventStoreConfig.Host (#11)
    * EventStoreConfig properties are now virtual so that default values can be overridden, v1.1.1
    
    * That didn't work out as expected - reverted changes and sealed EventStoreConfig, v1.1.2

--- HiP-EventStoreLib/EventStoreLlp/EventStoreConfig.cs
+++ HiP-EventStoreLib/EventStoreLlp/EventStoreConfig.cs
@@ -3,10 +3,11 @@
     /// <summary>
     /// Specifies the configuration fields that are required for <see cref="EventStoreService"/> to work.
     /// </summary>
-    public class EventStoreConfig
+    public sealed class EventStoreConfig
     {
         /// <summary>
         /// Endpoint of the Event Store.
+        /// Default value: "tcp://localhost:1113"
         /// 
         /// Examples:
         /// "tcp://localhost:1113",
@@ -15,7 +16,7 @@
         /// 
         /// See also: http://docs.geteventstore.com/dotnet-api/4.0.0/connecting-to-a-server/
         /// </summary>
-        public string Host { get; set; }
+        public string Host { get; set; } = "tcp://localhost:1113";
 
         /// <summary>
         /// Name of the event stream to read from and write to.
