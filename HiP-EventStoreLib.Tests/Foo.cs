aed15f79fddc3b0315ff60aaa4d9bd723f495cdc
Sven Vinkemeier <svenvinkemeier@live.de>
2018-01-10 15:32:57 +0100

    Removed unused attributes from testing code

--- HiP-EventStoreLib.Tests/Foo.cs
+++ HiP-EventStoreLib.Tests/Foo.cs
@@ -5,12 +5,11 @@ namespace PaderbornUniversity.SILab.Hip.EventSourcing.Tests
     public class Foo : IEntity<int>
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
 
-        [ResourceReferenceAttribute("Bar")]
         public int Parent { get; set; }
 
-        [ResourceReferenceAttribute("Bar")]
         public List<int> Bars { get; set; }
     }
 }
